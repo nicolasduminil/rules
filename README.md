3c6b3659f78fb494f8c24ad4f15de20fefe2fde7
Nicolas DUMINIL <nicolas.duminil@simplex-software.fr>
2022-03-04 15:53:20 +0100

    Have tested against WebLogic 14c

--- README.md
+++ README.md
@@ -2,24 +2,29 @@ This project shows how to use Drools, one of the most popular Business Rules Eng
 
 #Required dependencies:
 
-<dependency  
-  <groupIdorg.drools</groupId  
-  <artifactIddrools-compiler</artifactId  
-  <version7.5.0.Final</version  
-</dependency  
-<dependency  
-  <groupIdorg.drools</groupId  
-  <artifactIddrools-decisiontables</artifactId  
-  <version7.5.0.Final</version  
-</dependency  
-<dependency  
-  <groupIdorg.kie</groupId  
-  <artifactIdkie-ci</artifactId  
-  <version7.5.0.Final</version  
+```
+```
+
+```
+<dependency>
+  <groupIdorg.drools</groupId
+  <artifactIddrools-compiler</artifactId
+  <version7.5.0.Final</version
+</dependency
+<dependency
+  <groupIdorg.drools</groupId
+  <artifactIddrools-decisiontables</artifactId
+  <version7.5.0.Final</version
+</dependency
+<dependency
+  <groupIdorg.kie</groupId
+  <artifactIdkie-ci</artifactId
+  <version7.5.0.Final</version
 </dependency
 
-#Deploy, run and test  
-cd rules                                             #move to the home directory  
-mvn –DskipTests clean install                        #build the WAR file while skipping the test phase  
-mvn com.oracle.weblogic:weblogic-maven-plugin:deploy #deploy the WAR file on the local WebLogic server  
+#Deploy, run and test
+cd rules                                             #move to the home directory
+mvn –DskipTests clean install                        #build the WAR file while skipping the test phase
+mvn com.oracle.weblogic:weblogic-maven-plugin:deploy #deploy the WAR file on the local WebLogic server
 mvn test                                             #run the unit test  e
+```
--- src/test/java/de/telefonica/rules_management/TestFacade.java
+++ src/test/java/de/telefonica/rules_management/TestFacade.java
@@ -1,24 +1,55 @@
 package de.telefonica.rules_management;
 
-import java.util.*;
-
-import javax.naming.*;
-
-import org.junit.*;
-
 import de.telefonica.rules_management.facade.*;
 import de.telefonica.rules_management.facts.*;
 import de.telefonica.rules_management.facts.Customer.*;
+import org.junit.*;
+import org.kie.api.*;
+
+import javax.naming.*;
+import java.util.*;
+
+import static org.junit.Assert.*;
 
 public class TestFacade
 {
-  @Test
+  private KieFacadeRemote kieFacadeRemote;
+
+  @Before
   public void test() throws Exception
   {
-    Hashtable<String, String> props = new Hashtable<String, String>();
+    Hashtable<String, String> props = new Hashtable<>();
     props.put(Context.INITIAL_CONTEXT_FACTORY, "weblogic.jndi.WLInitialContextFactory");
-    props.put(Context.PROVIDER_URL, "t3://localhost:7001"); 
-    KieFacadeRemote facade = (KieFacadeRemote) new InitialContext(props).lookup("java:global.rules-manager.KieFacade!de.telefonica.rules_management.facade.KieFacadeRemote");
-    Assert.assertEquals(15, facade.fireRules(new Customer(CustomerType.INDIVIDUAL, 5)));
+    props.put(Context.PROVIDER_URL, "t3://localhost:7001");
+    kieFacadeRemote = (KieFacadeRemote) new InitialContext(props)
+      .lookup("java:global.rules-manager.KieFacade!de.telefonica.rules_management.facade.KieFacadeRemote");
+  }
+
+  @Test
+  public void givenRemoteKieIsNotNullThenSucceed()
+  {
+    assertNotNull(kieFacadeRemote);
+  }
+
+  @Test
+  public void givenLongDateIndividualCustomerWhenFireRuleThenCorrectDiscount()
+  {
+    Customer customer = new Customer(CustomerType.INDIVIDUAL, 5);
+    assertEquals(kieFacadeRemote.fireRules(customer), 15);
+  }
+
+  @Test
+  public void givenShortDateIndvidualCustomerWhenFireRuleThenCorrectDiscount()
+  {
+    Customer customer = new Customer(CustomerType.INDIVIDUAL, 1);
+    assertEquals(kieFacadeRemote.fireRules(customer), 5);
+  }
+
+  @Test
+  public void givenBusinessAnyWhenFireRuleThenCorrectDiscount()
+  {
+    Customer customer = new Customer(CustomerType.BUSINESS, 0);
+    kieFacadeRemote.fireRules(customer);
+    assertEquals(kieFacadeRemote.fireRules(customer), 20);
   }
 }
